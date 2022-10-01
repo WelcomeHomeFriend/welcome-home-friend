5874956276c35a0d31d54a510d69b53e4a3d9363
Krisette Odegard <kmodeg@gmail.com>
2022-09-30 19:32:19 -0700

    boilerplate code established

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
