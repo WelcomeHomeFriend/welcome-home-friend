b715d2f14876d0ed5a0fd968aca61ca860ed0281
Akash <akash.vinay.patel@gmail.com>
2022-10-01 20:39:24 -0700

    Cleaned up Some Code for Scalability

--- client/components/CreatePost.jsx
+++ client/components/CreatePost.jsx
@@ -6,51 +6,51 @@ const CreatePost = () => {
   // const petDetails = usePetContext()
   // _id (req), pet_name (req), owner, address, eye_color, gender, image_url, fur_color, last_found, comments
 
+  //NOTE: when invoking inputDev, make sure the second 'key' paramter matches a dataKey
+  //data keys are for the properties of the big object returned when we setContext
+  const inputDiv = (label, key) => { return (
+    <div>
+      <label>{label}</label>
+      <input id={key} type='text'></input>
+    </div>
+  )};
+
   return (
     <div className="create-post">
-      <div>
         <h2>Create a Post</h2>
 
         <h3>Your Information</h3>
-        <label for='your-name'>Your Name:</label>
-        <input id='name-input' type='text'></input>
-        <label for='address'>Address:</label>
-        <input id='address' type='text'></input>
+        {inputDiv('Your Name:', 'owner')}
+        {inputDiv('Address:', 'address')}
+        {inputDiv('Number:', 'phone_number')}
         
         <h3>Pet's Information</h3>
-        <label for pet-name>Name:</label>
-        <input id='pet-name' type='text'></input>
-        <label for='eye-color'>Eye Color:</label>
-        <input id='eye-color' type='text'></input>
-        <label for='gender'>Gender:</label>
-        <input id='gender' type='text'></input>
-        <label for='image'>Image url:</label>
-        <input id='image' type='text'></input>
-        <label for='fur-color'>Fur Color:</label>
-        <input id='fur-color' type='text'></input>
-        <label for='last-seen'>Last Seen:</label>
-        <input id='last-seen' type='text'></input>
-        <label for='comments'>Additional Comments:</label>
-        <input id='comments' type='text'></input>
-      </div>
-      
+        {inputDiv('Name:', 'pet_name')}
+        {inputDiv('Eye Color:', 'eye_color')}
+        {inputDiv('Gender:', 'gender')}
+        {inputDiv('Fur Color:', 'fur_color')}
+        {inputDiv('Last Seen:', 'last_found')}
+        {inputDiv('Additional Comments:', 'comments')}
       <button onClick={() => addPetArr(petData())}>Submit</button>
     </div>
-    /* form with inputs and button */ 
   )
 }
 
 //make a petData a function that returns an Object with all the data from the input fields of the DOM
 //make sure he data is formatted with correct key value pairs
+//NOTE: make sure elements of dataKey match with the second param of inputDiv
+const dataKeys = ['pet_name', 'owner', 'address', 'eye_color', 'gender', 'image_url', 'fur_color', 'last_found', 'comments', 'phone_number']
 const petData = () => {
-  return {
-    pet_name: document.getElementById('pet-name').value,
-    
-    // _id (req), pet_name (req), owner, address, eye_color, gender, image_url,
-    // fur_color: ,
-    // last_found: ,
-    comments: document.getElementById('comments').value
+  const dataObj = {}
+
+  for (let key of dataKeys) {
+    if (document.getElementById(key)) {
+      dataObj[key] = document.getElementById(key).value;
+      document.getElementById(key).value = '';
+    }
   }
+
+  return dataObj;
 }
 
 export default CreatePost;
