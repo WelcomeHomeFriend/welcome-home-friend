dc430a40ec090b32e6d70f361ba8983de107606d
Krisette Odegard <kmodeg@gmail.com>
2022-10-02 11:52:57 -0700

    added Jinx & Kobe to FeaturedPets component. styling in progress.

--- client/components/FeaturedPets.jsx
+++ client/components/FeaturedPets.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import kobe from '../images/kobe.jpg';
+import jinx from '../images/jinx.jpg';
 
 const FeaturedPets = () => {
   return (
     <div className="featured-pets">
-      <h1 className="center">Featured Pets</h1>
+      {/* Create a div for 2 pets showing PetDetails */}
+      <div className="featured-pet-title">
+      <h1>Featured Pets</h1>
+      </div>
+      <div className="featured-pet-details">
+        <div className="pet-image">
+        <img src={kobe} alt="Kobe" width="100px" />
+        </div>
+        <div className="featured-pet-info">
+          <h2>Kobe</h2>
+        </div>
+      </div>
+      <div className="featured-pet-details">
+        <div className="featured-pet-image">
+        <img src={jinx} alt="Jinx" width="100px" />
+        </div>
+        <div className="featured-pet-info">
+          <h2>Jinx</h2>
+          <p>Jinx likes treats and sleeping</p>
+          </div>
     </div>
-    /* Create a div for 2 pets showing PetDetails */ 
-  )
-}
+    </div> 
+  );
+};
 
 export default FeaturedPets;
