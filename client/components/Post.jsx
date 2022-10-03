1893e1dbe86cb838df9ae3ea03ef1a67cce9ea59
Krisette Odegard <kmodeg@gmail.com>
2022-10-03 16:05:37 -0700

    more styling, adjusted createpost/featured pets/post details

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "./PostModal.jsx"
+import pushpin from '../images/randoogle_Thumbtack_Pushpin.svg';
 
 const Post = ( {petObj} ) => {
   //petObj will be a giant object with key value pairs of:
@@ -7,19 +8,25 @@ const Post = ( {petObj} ) => {
       //eye_color, gender, image_url, fur_color, last_found, comments
 
   function loadImg(url) {
-    if (url) return <img height='75' width='75' src={petObj.image_url}></img>
+    if (url) return <img className="lost-pet-pic" src={petObj.image_url}></img>
   }
 
   return (
     <div className="post">
-      <h4><b>{petObj.pet_name}</b> is Lost</h4>
-      {loadImg(petObj.image_url)}
-      <p>Is a {petObj.type}</p>
-      <p>Has {petObj.fur_color} colored fur</p>
-      <p>Has {petObj.eye_color} colored eyes</p>
-      <p>Was last seen on/at? {petObj.last_found}</p>
-      <p>Owner is:{petObj.owner}</p>
+      <div className="petPic">
+        <img className="pushPin" src={pushpin} />
+      </div>
+      <h4 className="header4"><span class="petName">{petObj.pet_name.toUpperCase()}</span> is lost!</h4>
+      <div className="petPic">{loadImg(petObj.image_url)}</div>
+      <div className="info">
+        <p>Pet is a <b>{petObj.gender.toUpperCase()} {petObj.type.toUpperCase()}</b></p> {/* put type in line 15 or here? */}
+        <p>Has <b>{petObj.fur_color.toUpperCase()}</b> colored fur</p>
+        <p>Has <b>{petObj.eye_color.toUpperCase()}</b> colored eyes</p>
+        <p>Was last seen at? <b>{petObj.last_found.toUpperCase()}</b></p>
+        <p>Owner is: <b>{petObj.owner.toUpperCase()}</b></p>
       <Modal petObj={petObj}></Modal>
+      </div>
+    
     </div>
   )
 }
