80c670ede21c753577f1522ab28b6246982a3089
Akash <akash.vinay.patel@gmail.com>
2022-10-03 12:42:13 -0700

    Connected React to Server/DB

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -6,10 +6,15 @@ const Post = ( {petObj} ) => {
       // _id (req), pet_name (req), owner, address, 
       //eye_color, gender, image_url, fur_color, last_found, comments
 
+  function loadImg(url) {
+    if (url) return <img height='75' width='75' src={petObj.image_url}></img>
+  }
 
   return (
     <div className="post">
       <h4><b>{petObj.pet_name}</b> is Lost</h4>
+      {loadImg(petObj.image_url)}
+      <p>Is a {petObj.type}</p>
       <p>Has {petObj.fur_color} colored fur</p>
       <p>Has {petObj.eye_color} colored eyes</p>
       <p>Was last seen on/at? {petObj.last_found}</p>
