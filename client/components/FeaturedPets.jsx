effe5db8d6492ec7b66a29e155ffad7e54af452c
Krisette Odegard <kmodeg@gmail.com>
2022-10-03 17:03:33 -0700

    even more styling

--- client/components/FeaturedPets.jsx
+++ client/components/FeaturedPets.jsx
@@ -4,14 +4,15 @@ import jinx from '../images/jinx.jpg';
 
 const FeaturedPets = () => {
 
-  function foundDiv(name, imgSrc, description = '') {
+  function foundDiv(name, imgSrc, description = '', owner = '') {
     return (
         <div className="featured-pet-details">
-          <h2 className="featured-pet-name"><b>{name.toUpperCase()}</b></h2>
+          <h2 className="featured-pet-name"><b>Welcome home, {name.toUpperCase()}!</b></h2>
           <div style={{height:'100', width:'80%'}}>
             <img className="petPic" src={imgSrc}/>
           </div>
           <p className='featured-pet-description'>{description}</p>
+          <p><i>- {owner}</i></p>
         </div>
     )
   }
@@ -19,10 +20,10 @@ const FeaturedPets = () => {
   return (
     <div className="featured-pets">
       <h1 className="featured-pet-title">Found Friends</h1>
-      {foundDiv('Kobe', 'https://i.ibb.co/YTt3tqW/275649418-659368725316199-7217861032276615800-n-1.jpgibb.co/CsnznTt', 'He was found under the swing. Thank you Welcome Home Friend!')}
-      {foundDiv('Jinx', 'https://i.ibb.co/MDW5kzJ/Jinx-Krisette.jpg', 'OMG! Thank you, Welcome Home Friend! I was able to find my baby jinx!')}
-      {foundDiv('Butter', 'https://i.ibb.co/V2qfYnQ/Butter-Tim-Cousin.jpg', 'Booter')}
-      {foundDiv('Django', 'https://i.ibb.co/7GgtNmH/Capture.png', 'Honestly I\'ll probably end up making another post. He disappears every other day')}
+      {foundDiv('Kobe', 'https://i.ibb.co/YTt3tqW/275649418-659368725316199-7217861032276615800-n-1.jpgibb.co/CsnznTt', 'He was found under the swing. Thank you Welcome Home Friend!', 'Akash')}
+      {foundDiv('Jinx', 'https://i.ibb.co/MDW5kzJ/Jinx-Krisette.jpg', 'OMG! Thank you, Welcome Home Friend! I was able to find my baby jinx!', 'Krisette')}
+      {foundDiv('Butter', 'https://i.ibb.co/V2qfYnQ/Butter-Tim-Cousin.jpg', 'Booter', 'Tim')}
+      {foundDiv('Django', 'https://i.ibb.co/7GgtNmH/Capture.png', 'Honestly I\'ll probably end up making another post. He disappears every other day', 'Hannah')}
     </div>
   );
 };
