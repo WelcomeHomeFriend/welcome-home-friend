a746d504013d8018dad58516b987dd76cd1acfa4
Chance <kupersmith3.6@gmail.com>
2022-10-04 18:19:40 -0500

    clean

--- client/index.js
+++ client/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 
-render(
-    <App>
-    </App>,
-    document.getElementById('root')
-);
+// render(
+//     <App>
+//     </App>,
+//     document.getElementById('root')
+// );
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+    <App/>
+)
