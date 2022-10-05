89636f50ccddaa9123916cd43fa9cbe9c290cd43
Mark Nichting <mtnichting@gmail.com>
2022-10-05 14:05:48 -0700

    added front end functionality to add a pin to the map

---
+++ client/components/Marker.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect, useRef } from "react";
+
+
+const Marker = (options) => {
+  const [marker, setMarker] = useState();
+
+  useEffect(() => {
+    console.log(options);
+    if (!marker) {
+      // when this is first called, map is undefined because we need to first pass the array of <Marker/> to Map first, therefore we are not adding the position and map from props here
+      setMarker(new google.maps.Marker());
+    }
+    // remove marker from map on unmount
+    return () => {
+      if (marker) {
+        marker.setMap(null);
+      }
+    };
+  }, [marker]);
+
+  // second use effect will be triggered once a marker has been created to set market options
+  useEffect(() => {
+    if (marker) {
+      marker.setOptions(options);
+    }
+  }, [marker, options]);
+   
+  return null;
+};
+
+
+
+export default Marker; 
--- client/containers/MapContainer.jsx
+++ client/containers/MapContainer.jsx
@@ -5,6 +5,8 @@ import React from "react";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 // importing files 
 import Map from '../components/Map.jsx';
+import Marker from "../components/Marker.jsx";
+
 
 
 const render = (status) => {
@@ -12,10 +14,22 @@ const render = (status) => {
 };
 
 const MapContainer = () => {
+  const options = {
+    position: {lat: 34.052, lng: -118.244}
+  }
+
+  const arrayOfMarkers = [];
+
+  const getArray = () => {
+    arrayOfMarkers.push(<Marker options={options} />)
+    return arrayOfMarkers
+  }
 
   return(
     <Wrapper apiKey={"AIzaSyAB2C2iamCoJLJyVjkxxI2X3wTC-OoI6Vk"} render={render}>
-      <Map zoom={10} center={{lat: 34.052, lng: -118.244}}/>
+      <Map zoom={10} center={options.position} arrayOfMarkers={getArray()}>
+        {arrayOfMarkers}
+      </Map>
     </Wrapper>
   )
 };
