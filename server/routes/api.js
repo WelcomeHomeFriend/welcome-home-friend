540bc0bd7dd995843bce26bc6f1e1bd9eaa53c9f
Tracy Chang <Tracy@Andrews-MacBook-Air.local>
2022-10-05 08:56:33 -0700

    reformatted server api

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,30 +6,45 @@ const UserController = require('../controllers/UserController');
 const router = express.Router();
 
 
-router.get('/', UserController.checkCookie, petController.getPet, (req, res) => {
-    return res.status(200).json(res.locals.rows) // place holders -> looks like i'll be using query instead or maybe in the controllers?
+router.get('/', 
+    UserController.checkCookie, 
+    petController.getPet, 
+    (req, res) => {
+    return res.status(200).json(res.locals.rows) 
 });
 
 
-router.post('/pet', petController.addPet, (req, res) => {
-    return res.status(200).json(res.locals.newPet); // place holders
+router.post('/pet', 
+    petController.addPet, 
+    (req, res) => {
+    return res.status(200).json(res.locals.newPet); 
 });
 
-router.post('/found',   (req, res) => {
-    return res.status(200).json(res.locals.newPet); // place holders
+router.post('/found',   
+    (req, res) => {
+    return res.status(200).json(res.locals.newPet); 
 });
 
 
-router.post('/signup', UserController.verifyUser, UserController.createUser, UserController.setCookie, (res, req) => {
+router.post('/signup', 
+    UserController.verifyUser, 
+    UserController.createUser, 
+    UserController.setCookie, 
+    (res, req) => {
     return res.status(200).json({});
 });
 
 
-router.post('/login', UserController.loginUser, UserController.setCookie, (res, req) => {
+router.post('/login', 
+    UserController.loginUser, 
+    UserController.setCookie, 
+    (res, req) => {
     res.status(200).json({})
 });
 
-router.post('/logout', UserController.logoutUser, (res, req) => {
+router.post('/logout', 
+    UserController.logoutUser, 
+    (res, req) => {
     res.status(200).json({})
 });
 
