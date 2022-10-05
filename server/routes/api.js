9de11fcccbad082c7d43038af7a3bc2eb880272b
Tony Diethelm <tonydiethelm@gmail.com>
2022-10-05 10:48:15 -0700

    Working on adding BE routes for Oauth

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,19 +5,26 @@ const petController = require('../controllers/petController');
 const router = express.Router();
 
 
-router.get('/', petController.getPet, (req, res) => {
+router.get('/', 
+petController.getPet, 
+(req, res) => {
     return res.status(200).json(res.locals.rows) // place holders -> looks like i'll be using query instead or maybe in the controllers?
 });
 
 
-router.post('/pet', petController.addPet, (req, res) => {
+router.post('/pet', 
+petController.addPet, 
+(req, res) => {
     return res.status(200).json(res.locals.newPet); // place holders
 });
 
-router.post('/found', petController.foundPet, (req, res) => {
+router.post('/found', 
+petController.foundPet, 
+(req, res) => {
     return res.status(200).json(res.locals.newPet); // place holders
 });
 
+router.post('/v1/auth/google', )
 
 
 module.exports = router;
