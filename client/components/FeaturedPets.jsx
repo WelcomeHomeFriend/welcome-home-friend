293601389eb698cca2d7efe010db005ced47faa0
Tony Diethelm <tonydiethelm@gmail.com>
2022-10-06 16:34:44 -0700

    new Found Friends

--- client/components/FeaturedPets.jsx
+++ client/components/FeaturedPets.jsx
@@ -20,10 +20,12 @@ const FeaturedPets = () => {
   return (
     <div className="featured-pets">
       <h1 className="featured-pet-title">Found Friends</h1>
-      {foundDiv('Kobe', 'https://i.ibb.co/YTt3tqW/275649418-659368725316199-7217861032276615800-n-1.jpgibb.co/CsnznTt', 'He was found under the swing. Thank you Welcome Home Friend!', 'Akash')}
-      {foundDiv('Jinx', 'https://i.ibb.co/MDW5kzJ/Jinx-Krisette.jpg', 'OMG! Thank you, Welcome Home Friend! I was able to find my baby jinx!', 'Krisette')}
-      {foundDiv('Butter', 'https://i.ibb.co/V2qfYnQ/Butter-Tim-Cousin.jpg', 'Booter', 'Tim')}
-      {foundDiv('Django', 'https://i.ibb.co/7GgtNmH/Capture.png', 'Honestly I\'ll probably end up making another post. He disappears every other day', 'Hannah')}
+      {foundDiv('Dancer', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Dendroaspis_polylepis_%2814%29.jpg/1200px-Dendroaspis_polylepis_%2814%29.jpg', 'He was found in the mall. Thank you Welcome Home Friend!', 'Tony')}
+      {foundDiv('Monaco', 'https://www.monaconatureencyclopedia.com/wp-content/uploads/2008/08/jpg_Il_Bungarus_fasciatus_e_velenosissimo_ma_in_compenso_poco_aggressivo_di_giorno_e_di_notte_uccide_i_cobra_c_Mazza.jpg', 'Thank you, Welcome Home Friend!', 'Alex Martinez')}
+      {foundDiv('Russel', 'https://upload.wikimedia.org/wikipedia/commons/7/74/Daboia_russelii_A_Chawla01.jpg', 'Thank you, she is home safe!', 'Zi')}
+      {foundDiv('Ratty', 'https://www.marylandzoo.org/wp-content/uploads/2017/10/black_rat_snake_web.jpg', 'Thank you for finding my beloved Ratty!', 'Mark')}
+      {foundDiv('Jingles', 'https://www.cdc.gov/niosh/topics/snakes/images/rattlesnake.jpg?_=88099', 'Thank you Welcome Home Friend!', 'Richard')}
+    
     </div>
   );
 };
